50d211b05a2cd19928901e2cd6f55e03d07f798c
FloHGer <mail@florianhoehle.de>
2021-06-08 08:58:19 +0200

    init

---
+++ js.js
@@ -0,0 +1,21 @@
+let quotes = ['q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10'];
+
+// Math.random() * quotes.length                                        1) generate random number
+// Math.floor( Math.random() * quotes.length )                          2) transform random number to integer
+// quotes[ Math.floor( Math.random() * quotes.length ) ]                3) use random integer as index of an array
+console.log( quotes[ Math.floor( Math.random() * quotes.length ) ] ); //4) print the random index of the array
+
+
+
+let text = 'Hello World';
+          //012345678901
+
+console.log(text.substring(6, 11)); // 'World'
+
+
+
+// letters = 'ABCabc';
+// numbers = '123';
+// chars = '!@#';
+allChars = 'ABCabc124!@#';
+
